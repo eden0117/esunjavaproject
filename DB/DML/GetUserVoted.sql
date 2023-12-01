42d11eaa1899e18465edc621157f113739f7ff32
eden <eden460793@gmail.com>
2023-12-02 02:43:38 +0800

    v2023.12.02.v4

--- DB/DML/GetUserVoted.sql
+++
@@ -1,4 +0,0 @@
-CREATE PROCEDURE `GetUserVoted` (in p_userid int )
-BEGIN
-SELECT voteitems.idvote, voteitems.voteName FROM voteitems WHERE voteitems.idvote IN ( SELECT voterecord.idVote FROM voterecord WHERE voterecord.idVoter = 2);
-END
