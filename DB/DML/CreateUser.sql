985e80a9a1e16fc5e60e5e5794a8c697c0b98993
eden <eden460793@gmail.com>
2023-12-02 14:54:58 +0800

    v2023.12.02-update-createuser-sp

--- DB/DML/CreateUser.sql
+++ DB/DML/CreateUser.sql
@@ -1,5 +1,16 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `CreateUser`(in username varchar(45),in userpassword varchar(45))
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CreateUser`(IN username VARCHAR(45), IN userpassword VARCHAR(45), OUT result INT)
 BEGIN
-INSERT INTO voterlist (`Name`) VALUES (username);
-INSERT INTO `mydb`.`password` (`password`) VALUES (userpassword);
+    IF NOT EXISTS (
+        SELECT 1
+        FROM voterlist
+        INNER JOIN `password` ON voterlist.idVoter = `password`.idpassword  
+        WHERE voterlist.Name = username AND `password`.`password` = userpassword
+    ) THEN
+        INSERT INTO voterlist (`Name`) VALUES (username);
+        INSERT INTO `mydb`.`password` (`password`) VALUES (userpassword);
+        SET result = 1;  -- 表示創建成功
+    ELSE
+        SET result = 0;  -- 表示用戶已存在
+    END IF;
+    select result;
 END
